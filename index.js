6bdf6606dd08649056a32a6a94d689b913007e82
Mohammad Arif <arif_iq@yahoo.co.in>
2023-01-08 17:26:52 +0530

    Add the pipe

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { Observable } = require('rxjs');
+const { map } = require('rxjs/operators');
 
 const users = {
   data: [
     {
       status: 'active',
-      age: 14,
+      age: 15,
     },
     {
       status: 'inactive',
@@ -38,12 +39,39 @@ const users = {
 
 const observable = new Observable((subscriber) => {
   // Emit a value
-  subscriber.next(10); // value emitted to the observer
-  subscriber.next(11); // value emitted to the observer
-  subscriber.next(12); // value emitted to the observer
-});
+  subscriber.next(users); // value emitted to the observer
+}).pipe(
+  // First map operator to get the data from the object
+  map((value) => {
+    console.log('1) Got data from observable', value);
+    return value.data;
+  }),
 
-// Create an observer
+  // Second map operator to filter the data
+  map((value) => {
+    console.log('2) Got data from first operator', value);
+    return value.filter((user) => user.status === 'active');
+  }),
+
+  // Third map operator to get the sum of the ages
+  map((value) => {
+    console.log('3) Got data from first operator', value);
+    return value.reduce((sum, user) => {
+      return sum + user.age;
+    }, 0);
+  }),
+
+  // Fourth map operator to throw an error if the age is less than 18
+  map((value) => {
+    console.log('4) Got data from third operator', value);
+    if (value < 115) {
+      throw new Error('Age is less than 18');
+    }
+    return value;
+  })
+);
+
+// Create an observer to handle the emitted values
 const observer = {
   next: (value) => {
     // Handle the emitted value
